eeac260c5b83733f2883ee32e6f966ce400fb963
Nella2912 <63076368+Nella2912@users.noreply.github.com>
2020-05-12 10:56:42 +0200

    Add files via upload

---
+++ Arbre.java
@@ -0,0 +1,15 @@
+/**
+ * modelise un arbre avec la méthode UPGMA
+ * @author Hornella Fosso-Kahina Lounaci
+ *
+ */
+public class Arbre {
+	int [][] mD;               // une matrice de distances prise en paramètre
+	
+	public Arbre(int [][] matDeDist) {
+		this.mD = matDeDist;
+	}
+	
+	
+	//public void upgma
+}
