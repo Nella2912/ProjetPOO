324389ddee635f277a6cf06547ea25c9a43b2f0f
LOUNACI <63150656+LOUNACI@users.noreply.github.com>
2020-05-28 12:16:48 +0200

    Update Arbre.java

--- Arbre.java
+++ Arbre.java
@@ -29,7 +29,38 @@ public class Arbre {
 		this.nbreFeuilles = 1;
 		this.pere = null;
 	}
-	
+	public Alignement parcourArbre() {
+		/*
+		Arbre A =new Arbre();
+		A=listeArbres.get(0); recupere l'arbre en entier construit
+		*/
+		Arbre A1 = this.gauche;
+		Arbre A2 = this.droite;
+		
+		System.out.println("rien");
+		if(A1.gauche==null){System.out.println("merde1");}
+		if(A2==null){System.out.println("merde2");}
+		
+		if(A1 ==null && A2==null){
+			
+			Alignement AS = new Alignement(A1.pere.seqCle,A2.pere.seqCle);
+			AS.aligner(); // alignement au niveau des feuilles
+			System.out.println("merdeif");
+			return AS;
+		}
+		else {
+			System.out.println("merde");
+			Alignement AS1 = A1.parcourArbre();
+			
+			AS1.aligner();
+			
+			Alignement AS2 = A2.parcourArbre();
+			AS2.aligner();
+			Alignement AS = new Alignement(AS1,AS2);
+		
+			return AS;
+		}
+	}	
 	/**
 	 * constructeur arbre avec deux arbres en paramètres
 	 * @param id
