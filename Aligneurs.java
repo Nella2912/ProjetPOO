336c0dcb21eb5ea4c6e5ba1f089107a2bbe7c6d8
Nella2912 <63076368+Nella2912@users.noreply.github.com>
2020-06-14 22:25:24 +0200

    Delete Aligneurs.java

--- Aligneurs.java
+++
@@ -1,8 +0,0 @@
-/**
- * Interface de modelisation des alignements (Sequence => Sequence, Alignement<=> Sequence, Alignement <=> Alignement)
- * @author Hornella Fosso-Kahina Lounaci
- *
- */
-public interface Aligneurs {
-	public void matriceDeScore();
-}
